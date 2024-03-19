b08e6010d73ced2196379c04d36b647627ec7ba8
declanqc <C20465442@mytudublin.ie>
2024-03-19 22:13:54 +0000

    spawner tweaks

--- CoffeeBot/Assets/Scripts/NPCStates/NpcSpawnerTesting.cs
+++ CoffeeBot/Assets/Scripts/NPCStates/NpcSpawnerTesting.cs
@@ -8,6 +8,8 @@ public class NpcSpawnerTesting : MonoBehaviour
     public Transform Spawnpoint;
     public int NpcSpawnTime = 10;
     public int NpcCount = 1;
+
+    public SceneInfo sceneInfo;
     public void Start()
     {
         Instantiate(NPC, new Vector3(Spawnpoint.position.x, Spawnpoint.position.y, Spawnpoint.position.z), Quaternion.identity);
@@ -16,7 +18,24 @@ public class NpcSpawnerTesting : MonoBehaviour
 
     public void Update()
     {
-
+        switch(sceneInfo.dayCount)
+        {
+            case 1:
+                NpcSpawnTime = 40;
+                break;
+            case 2:
+                NpcSpawnTime = 30;
+                break;
+            case 3:
+                NpcSpawnTime = 25;
+                break;
+            case 4:
+                NpcSpawnTime = 20;
+                break;
+            case 5:
+                NpcSpawnTime = 15;
+                break;
+        }
     }
     public IEnumerator SpawnNPC()
     {
