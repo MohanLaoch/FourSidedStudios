ee3384fe99ebab5c828b7e74a5061e32e6f5318c
declanqc <C20465442@mytudublin.ie>
2024-04-03 17:51:12 +0100

    added shaking effect to gumball machine

--- CoffeeBot/Assets/GumballMachine.cs
+++ CoffeeBot/Assets/GumballMachine.cs
@@ -14,6 +14,28 @@ public class GumballMachine : MonoBehaviour
     public bool MachineIsCooldown;
 
     public TextMeshProUGUI TimerText;
+
+
+
+
+    [Header("Info")]
+    private Vector3 _startPos;
+    private float _timer;
+    private Vector3 _randomPos;
+
+    [Header("Settings")]
+    [Range(0f, 2f)]
+    public float _time = 0.2f;
+    [Range(0f, 2f)]
+    public float _distance = 0.1f;
+    [Range(0f, 0.1f)]
+    public float _delayBetweenShakes = 0f;
+
+    public void Awake()
+    {
+        _startPos = transform.position;
+    }
+
     public void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
@@ -48,6 +70,9 @@ public class GumballMachine : MonoBehaviour
             TimerText.gameObject.SetActive(true);
             TimerText.text = GumballTimer.ToString("0");
             GumballTime = true;
+           
+
+
         }
         else
         {
@@ -60,12 +85,50 @@ public class GumballMachine : MonoBehaviour
 
     public void ApplyMachineCooldown()
     {
-       GumballTimer -= Time.deltaTime;
-
+        GumballTimer -= Time.deltaTime;
+        Begin();
         if(GumballTimer < 0.0f)
         {
             MachineIsCooldown = false;
             GumballTime = false;
+            StopAllCoroutines();
         }
     }
+    private void OnValidate()
+    {
+        if (_delayBetweenShakes > _time)
+            _delayBetweenShakes = _time;
+    }
+
+    public void Begin()
+    {
+        StopAllCoroutines();
+        StartCoroutine(Shake());
+    }
+
+    private IEnumerator Shake()
+    {
+        _timer = 0f;
+
+        while (_timer < _time)
+        {
+            _timer += Time.deltaTime;
+
+            _randomPos = _startPos + (Random.insideUnitSphere * _distance);
+
+            transform.position = _randomPos;
+
+            if (_delayBetweenShakes > 0f)
+            {
+                yield return new WaitForSeconds(_delayBetweenShakes);
+            }
+            else
+            {
+                yield return null;
+            }
+        }
+
+        transform.position = _startPos;
+    }
+
 }
