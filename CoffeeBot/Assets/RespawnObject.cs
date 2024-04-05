9097a5f20f339b8fa6109e3a838a0ffa9ae72949
James Mohan <c20305011@mytudublin.ie>
2024-04-05 15:13:24 +0100

    idiot proofing the tutorial scene

---
+++ CoffeeBot/Assets/RespawnObject.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnObject : MonoBehaviour
+{
+    public GameObject fallenObject;
+
+    private GameObject player;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Sergio");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        fallenObject = other.gameObject;
+
+        Instantiate(fallenObject, new Vector3(player.transform.position.x, player.transform.position.y + 2, player.transform.position.z), Quaternion.identity);
+
+        fallenObject = null;
+    }
+}
