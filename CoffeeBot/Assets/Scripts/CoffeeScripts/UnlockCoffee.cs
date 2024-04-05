f9ed49e0be701de5f5260bc21b883aaf291dfe25
declanqc <C20465442@mytudublin.ie>
2024-04-05 19:30:47 +0100

    coffees have a cost

--- CoffeeBot/Assets/Scripts/CoffeeScripts/UnlockCoffee.cs
+++ CoffeeBot/Assets/Scripts/CoffeeScripts/UnlockCoffee.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UnlockCoffee : MonoBehaviour
@@ -48,184 +49,226 @@ public class UnlockCoffee : MonoBehaviour
     public GameObject tea;
     public Sprite teaSprite;
 
+    [Header("MoneyText")]
+    public TextMeshProUGUI MoneyText;
+
     public void UnlockChaiLatte()
     {
-        craftCoffee.recipes.Add("MilkTeaBagsNull");
-        craftCoffee.recipes.Add("MilkNullTeaBags");
-        craftCoffee.recipes.Add("TeaBagsMilkNull");
-        craftCoffee.recipes.Add("TeaBagsNullMilk");
-        craftCoffee.recipes.Add("NullMilkTeaBags");
-        craftCoffee.recipes.Add("NullTeaBagsMilk");
-
-        for (int i = 0; i < 6; i++)
+        if(sceneInfo.money >= 15)
         {
-            craftCoffee.recipeResults.Add(chaiLatte);
+            sceneInfo.money -= 15;
+            MoneyText.text = ": " + sceneInfo.money.ToString("0");
 
-            craftCoffee.recipeImages.Add(chaiLatteSprite);
+            craftCoffee.recipes.Add("MilkTeaBagsNull");
+            craftCoffee.recipes.Add("MilkNullTeaBags");
+            craftCoffee.recipes.Add("TeaBagsMilkNull");
+            craftCoffee.recipes.Add("TeaBagsNullMilk");
+            craftCoffee.recipes.Add("NullMilkTeaBags");
+            craftCoffee.recipes.Add("NullTeaBagsMilk");
 
-            craftCoffee.recipeNames.Add("Chai Latte");
+            for (int i = 0; i < 6; i++)
+            {
+                craftCoffee.recipeResults.Add(chaiLatte);
+
+                craftCoffee.recipeImages.Add(chaiLatteSprite);
+
+                craftCoffee.recipeNames.Add("Chai Latte");
+            }
+
+            if (!hasTeaBags)
+            {
+                Instantiate(teaBags, new Vector3(teaBagsSpawn.position.x, teaBagsSpawn.position.y, teaBagsSpawn.position.z), Quaternion.identity);
+                hasTeaBags = true;
+            }
+
+            avaliableOrders.coffeeOrders.Add("Chai Latte");
+            avaliableOrders.orderIcons.Add(chaiLatteSprite);
+
+            sceneInfo.ChaiLatteUnlocked = true;
         }
-
-        if (!hasTeaBags)
-        {
-            Instantiate(teaBags, new Vector3(teaBagsSpawn.position.x, teaBagsSpawn.position.y, teaBagsSpawn.position.z), Quaternion.identity);
-            hasTeaBags = true;
-        }
-
-        avaliableOrders.coffeeOrders.Add("Chai Latte");
-        avaliableOrders.orderIcons.Add(chaiLatteSprite);
-
-        sceneInfo.ChaiLatteUnlocked = true;
+       
     }
 
     public void UnlockHotChocolate()
     {
-        craftCoffee.recipes.Add("MilkCocoPowderNull");
-        craftCoffee.recipes.Add("MilkNullCocoPowder");
-        craftCoffee.recipes.Add("CocoPowderMilkNull");
-        craftCoffee.recipes.Add("CocoPowderNullMilk");
-        craftCoffee.recipes.Add("NullMilkCocoPowder");
-        craftCoffee.recipes.Add("NullCocoPowderMilk");
-
-        for (int i = 0;  i < 6; i++)
+        if(sceneInfo.money >= 15)
         {
-            craftCoffee.recipeResults.Add(hotChocolate);
+            sceneInfo.money -= 15;
+            MoneyText.text = ": " + sceneInfo.money.ToString("0");
 
-            craftCoffee.recipeImages.Add(hotChocolateSprite);
+            craftCoffee.recipes.Add("MilkCocoPowderNull");
+            craftCoffee.recipes.Add("MilkNullCocoPowder");
+            craftCoffee.recipes.Add("CocoPowderMilkNull");
+            craftCoffee.recipes.Add("CocoPowderNullMilk");
+            craftCoffee.recipes.Add("NullMilkCocoPowder");
+            craftCoffee.recipes.Add("NullCocoPowderMilk");
 
-            craftCoffee.recipeNames.Add("Hot Chocolate");
+            for (int i = 0; i < 6; i++)
+            {
+                craftCoffee.recipeResults.Add(hotChocolate);
+
+                craftCoffee.recipeImages.Add(hotChocolateSprite);
+
+                craftCoffee.recipeNames.Add("Hot Chocolate");
+            }
+
+            if (!hasCocoPowder)
+            {
+                Instantiate(cocoPowder, new Vector3(cocoPowderSpawn.position.x, cocoPowderSpawn.position.y, cocoPowderSpawn.position.z), Quaternion.identity);
+                hasCocoPowder = true;
+            }
+
+            avaliableOrders.coffeeOrders.Add("Hot Chocolate");
+            avaliableOrders.orderIcons.Add(hotChocolateSprite);
+
+            sceneInfo.HotChocolateUnlocked = true;
         }
-
-        if (!hasCocoPowder)
-        {
-            Instantiate(cocoPowder, new Vector3(cocoPowderSpawn.position.x, cocoPowderSpawn.position.y, cocoPowderSpawn.position.z), Quaternion.identity);
-            hasCocoPowder = true;
-        }
-
-        avaliableOrders.coffeeOrders.Add("Hot Chocolate");
-        avaliableOrders.orderIcons.Add(hotChocolateSprite);
-
-        sceneInfo.HotChocolateUnlocked = true;
+        
     }
 
     public void UnlockIcedCoffee()
     {
-        craftCoffee.recipes.Add("IceCoffeeBeansWater");
-        craftCoffee.recipes.Add("IceWaterCoffeeBeans");
-        craftCoffee.recipes.Add("CoffeeBeansIceWater");
-        craftCoffee.recipes.Add("CoffeeBeansWaterIce");
-        craftCoffee.recipes.Add("WaterIceCoffeeBeans");
-        craftCoffee.recipes.Add("WaterCoffeeBeansIce");
-
-        for (int i = 0; i < 6; i++)
+        if(sceneInfo.money >= 15)
         {
-            craftCoffee.recipeResults.Add(icedCoffee);
+            sceneInfo.money -= 15;
+            MoneyText.text = ": " + sceneInfo.money.ToString("0");
 
-            craftCoffee.recipeImages.Add(icedCoffeeSprite);
+            craftCoffee.recipes.Add("IceCoffeeBeansWater");
+            craftCoffee.recipes.Add("IceWaterCoffeeBeans");
+            craftCoffee.recipes.Add("CoffeeBeansIceWater");
+            craftCoffee.recipes.Add("CoffeeBeansWaterIce");
+            craftCoffee.recipes.Add("WaterIceCoffeeBeans");
+            craftCoffee.recipes.Add("WaterCoffeeBeansIce");
 
-            craftCoffee.recipeNames.Add("Iced Coffee");
+            for (int i = 0; i < 6; i++)
+            {
+                craftCoffee.recipeResults.Add(icedCoffee);
+
+                craftCoffee.recipeImages.Add(icedCoffeeSprite);
+
+                craftCoffee.recipeNames.Add("Iced Coffee");
+            }
+
+            if (!hasIce)
+            {
+                Instantiate(ice, new Vector3(iceSpawn.position.x, iceSpawn.position.y, iceSpawn.position.z), Quaternion.identity);
+                hasIce = true;
+            }
+
+            avaliableOrders.coffeeOrders.Add("Iced Coffee");
+            avaliableOrders.orderIcons.Add(icedCoffeeSprite);
+
+            sceneInfo.IcedCoffeeUnlocked = true;
         }
-
-        if (!hasIce)
-        {
-            Instantiate(ice, new Vector3(iceSpawn.position.x, iceSpawn.position.y, iceSpawn.position.z), Quaternion.identity);
-            hasIce = true;
-        }
-
-        avaliableOrders.coffeeOrders.Add("Iced Coffee");
-        avaliableOrders.orderIcons.Add(icedCoffeeSprite);
-
-        sceneInfo.IcedCoffeeUnlocked = true;
+       
     }
 
     public void UnlockIcedLatte()
     {
-        craftCoffee.recipes.Add("MilkCoffeeBeansIce");
-        craftCoffee.recipes.Add("MilkIceCoffeeBeans");
-        craftCoffee.recipes.Add("CoffeeBeansMilkIce");
-        craftCoffee.recipes.Add("CoffeeBeansIceMilk");
-        craftCoffee.recipes.Add("IceMilkCoffeeBeans");
-        craftCoffee.recipes.Add("IceCoffeeBeansMilk");
-
-        for (int i = 0; i < 6; i++)
+        if (sceneInfo.money >= 15)
         {
-            craftCoffee.recipeResults.Add(icedLatte);
+            sceneInfo.money -= 15;
+            MoneyText.text = ": " + sceneInfo.money.ToString("0");
 
-            craftCoffee.recipeImages.Add(icedLatteSprite);
+            craftCoffee.recipes.Add("MilkCoffeeBeansIce");
+            craftCoffee.recipes.Add("MilkIceCoffeeBeans");
+            craftCoffee.recipes.Add("CoffeeBeansMilkIce");
+            craftCoffee.recipes.Add("CoffeeBeansIceMilk");
+            craftCoffee.recipes.Add("IceMilkCoffeeBeans");
+            craftCoffee.recipes.Add("IceCoffeeBeansMilk");
 
-            craftCoffee.recipeNames.Add("Iced Latte");
+            for (int i = 0; i < 6; i++)
+            {
+                craftCoffee.recipeResults.Add(icedLatte);
+
+                craftCoffee.recipeImages.Add(icedLatteSprite);
+
+                craftCoffee.recipeNames.Add("Iced Latte");
+            }
+
+            if (!hasIce)
+            {
+                Instantiate(ice, new Vector3(iceSpawn.position.x, iceSpawn.position.y, iceSpawn.position.z), Quaternion.identity);
+                hasIce = true;
+            }
+
+            avaliableOrders.coffeeOrders.Add("Iced Latte");
+            avaliableOrders.orderIcons.Add(icedLatteSprite);
+
+            sceneInfo.IcedLatteUnlocked = true;
         }
-
-        if (!hasIce)
-        {
-            Instantiate(ice, new Vector3(iceSpawn.position.x, iceSpawn.position.y, iceSpawn.position.z), Quaternion.identity);
-            hasIce = true;
-        }
-
-        avaliableOrders.coffeeOrders.Add("Iced Latte");
-        avaliableOrders.orderIcons.Add(icedLatteSprite);
-
-        sceneInfo.IcedLatteUnlocked = true;
     }
 
     public void UnlockMocha()
     {
-        craftCoffee.recipes.Add("MilkCoffeeBeansCocoPowder");
-        craftCoffee.recipes.Add("MilkCocoPowderCoffeeBeans");
-        craftCoffee.recipes.Add("CoffeeBeansMilkCocoPowder");
-        craftCoffee.recipes.Add("CoffeeBeansCocoPowderMilk");
-        craftCoffee.recipes.Add("CocoPowderMilkCoffeeBeans");
-        craftCoffee.recipes.Add("CocoPowderCoffeeBeansMilk");
-
-        for (int i = 0; i < 6; i++)
+        if (sceneInfo.money >= 15)
         {
-            craftCoffee.recipeResults.Add(mocha);
+            sceneInfo.money -= 15;
+            MoneyText.text = ": " + sceneInfo.money.ToString("0");
 
-            craftCoffee.recipeImages.Add(mochaSprite);
+            craftCoffee.recipes.Add("MilkCoffeeBeansCocoPowder");
+            craftCoffee.recipes.Add("MilkCocoPowderCoffeeBeans");
+            craftCoffee.recipes.Add("CoffeeBeansMilkCocoPowder");
+            craftCoffee.recipes.Add("CoffeeBeansCocoPowderMilk");
+            craftCoffee.recipes.Add("CocoPowderMilkCoffeeBeans");
+            craftCoffee.recipes.Add("CocoPowderCoffeeBeansMilk");
 
-            craftCoffee.recipeNames.Add("Mocha");
+            for (int i = 0; i < 6; i++)
+            {
+                craftCoffee.recipeResults.Add(mocha);
+
+                craftCoffee.recipeImages.Add(mochaSprite);
+
+                craftCoffee.recipeNames.Add("Mocha");
+            }
+
+            if (!hasCocoPowder)
+            {
+                Instantiate(cocoPowder, new Vector3(cocoPowderSpawn.position.x, cocoPowderSpawn.position.y, cocoPowderSpawn.position.z), Quaternion.identity);
+                hasCocoPowder = true;
+            }
+
+            avaliableOrders.coffeeOrders.Add("Mocha");
+            avaliableOrders.orderIcons.Add(mochaSprite);
+
+            sceneInfo.MochaUnlocked = true;
         }
-
-        if (!hasCocoPowder)
-        {
-            Instantiate(cocoPowder, new Vector3(cocoPowderSpawn.position.x, cocoPowderSpawn.position.y, cocoPowderSpawn.position.z), Quaternion.identity);
-            hasCocoPowder = true;
-        }
-
-        avaliableOrders.coffeeOrders.Add("Mocha");
-        avaliableOrders.orderIcons.Add(mochaSprite);
-
-        sceneInfo.MochaUnlocked = true;
     }
 
     public void UnlockTea()
     {
-        craftCoffee.recipes.Add("MilkWaterTeaBags");
-        craftCoffee.recipes.Add("MilkTeaBagsWater");
-        craftCoffee.recipes.Add("WaterMilkTeaBags");
-        craftCoffee.recipes.Add("WaterTeaBagsMilk");
-        craftCoffee.recipes.Add("TeaBagsMilkWater");
-        craftCoffee.recipes.Add("TeaBagsWaterMilk");
-
-        for (int i = 0; i < 6; i++)
+        if (sceneInfo.money >= 15)
         {
-            craftCoffee.recipeResults.Add(tea);
+            sceneInfo.money -= 15;
+            MoneyText.text = ": " + sceneInfo.money.ToString("0");
 
-            craftCoffee.recipeImages.Add(teaSprite);
+            craftCoffee.recipes.Add("MilkWaterTeaBags");
+            craftCoffee.recipes.Add("MilkTeaBagsWater");
+            craftCoffee.recipes.Add("WaterMilkTeaBags");
+            craftCoffee.recipes.Add("WaterTeaBagsMilk");
+            craftCoffee.recipes.Add("TeaBagsMilkWater");
+            craftCoffee.recipes.Add("TeaBagsWaterMilk");
 
-            craftCoffee.recipeNames.Add("Tea");
+            for (int i = 0; i < 6; i++)
+            {
+                craftCoffee.recipeResults.Add(tea);
+
+                craftCoffee.recipeImages.Add(teaSprite);
+
+                craftCoffee.recipeNames.Add("Tea");
+            }
+
+            if (!hasTeaBags)
+            {
+                Instantiate(teaBags, new Vector3(teaBagsSpawn.position.x, teaBagsSpawn.position.y, teaBagsSpawn.position.z), Quaternion.identity);
+                hasTeaBags = true;
+            }
+
+            avaliableOrders.coffeeOrders.Add("Tea");
+            avaliableOrders.orderIcons.Add(teaSprite);
+
+            sceneInfo.TeaUnlocked = true;
         }
-
-        if (!hasTeaBags)
-        {
-            Instantiate(teaBags, new Vector3(teaBagsSpawn.position.x, teaBagsSpawn.position.y, teaBagsSpawn.position.z), Quaternion.identity);
-            hasTeaBags = true;
-        }
-
-        avaliableOrders.coffeeOrders.Add("Tea");
-        avaliableOrders.orderIcons.Add(teaSprite);
-
-        sceneInfo.TeaUnlocked = true;
     }
 }
 
