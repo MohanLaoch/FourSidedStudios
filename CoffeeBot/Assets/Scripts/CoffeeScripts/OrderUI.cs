f7a7e1bb1b90a38e9821f12589aa168ab152606d
James Mohan <c20305011@mytudublin.ie>
2024-04-15 15:53:58 +0100

    cooking in progress

---
+++ CoffeeBot/Assets/Scripts/CoffeeScripts/OrderUI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderUI : MonoBehaviour
+{
+    public List<string> currentOrders = new List<string>();
+
+
+}
+
