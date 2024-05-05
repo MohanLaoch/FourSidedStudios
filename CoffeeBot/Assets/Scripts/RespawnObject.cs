a48735e7a71aa764a860b40dd204edbc7389e4c8
James Mohan <c20305011@mytudublin.ie>
2024-05-05 13:49:02 +0100

    out of bounds npc fix

--- CoffeeBot/Assets/Scripts/RespawnObject.cs
+++ CoffeeBot/Assets/Scripts/RespawnObject.cs
@@ -15,11 +15,18 @@ public class RespawnObject : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        fallenObject = other.gameObject;
+        if (gameObject.tag == "NPC")
+        {
+            fallenObject = null;
+        }
+        else
+        {
+            fallenObject = other.gameObject;
 
-        Instantiate(fallenObject, new Vector3(player.transform.position.x, player.transform.position.y + 2, player.transform.position.z - 2), Quaternion.identity);
+            Instantiate(fallenObject, new Vector3(player.transform.position.x, player.transform.position.y + 2, player.transform.position.z - 2), Quaternion.identity);
 
-        fallenObject = null;
+            fallenObject = null;
+        }
 
     }
 }
